7da5dcb972ea4f50a56a8308c7ca0e33189b1d7e
XJ <ianyian@gmail.com>
2021-06-26 09:13:28 +0800

    add file

---
+++ Restful-ApiKey-based/Controllers/SecretController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restful_ApiKey_based.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restful_ApiKey_based.Controllers
+{
+    [ApiKeyAuth]
+    public class SecretController : ControllerBase 
+    {
+        [HttpGet(template:"secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("i'm gooood");
+        }
+    }
+}
---
+++ Restful-ApiKey-based/Filters/ApiKeyAuthAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Restful_ApiKey_based.Filters
+{
+    [AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAuthAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string ApiKeyHeaderName = "ApiKey";
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            //before
+            //context.HttpContext.Request.Query["ApiKey"])
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var PotentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var configuration = context.HttpContext.RequestServices.GetRequiredService <IConfiguration>();
+            var apikey = configuration.GetValue<String>("ApiKey");
+
+
+            if (!apikey.Equals(PotentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+            }
+
+            await next();
+            //after
+        }
+    }
+}
